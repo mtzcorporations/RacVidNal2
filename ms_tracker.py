db94cab70ab3bef0c93971c6b3fddedf60fed071
mtzcorporations <matjaz.zupanic1@gmail.com>
2022-03-25 11:27:48 +0100

    dodatki

---
+++ ms_tracker.py
@@ -0,0 +1,32 @@
+from ex2_utils import Tracker
+import numpy as np
+
+class msTracker(Tracker):
+    def initialize(self, image, region):
+
+        if len(region) == 8:
+            x_ = np.array(region[::2])
+            y_ = np.array(region[1::2])
+            region = [np.min(x_), np.min(y_), np.max(x_) - np.min(x_) + 1, np.max(y_) - np.min(y_) + 1]
+
+        self.window = max(region[2], region[3]) * self.parameters.enlarge_factor
+
+        left = max(region[0], 0)
+        top = max(region[1], 0)
+
+        right = min(region[0] + region[2], image.shape[1] - 1)
+        bottom = min(region[1] + region[3], image.shape[0] - 1)
+
+        self.template = image[int(top):int(bottom), int(left):int(right)]
+        self.position = (region[0] + region[2] / 2, region[1] + region[3] / 2)
+        self.size = (region[2], region[3])
+
+    def MeanShiftSeek(self):
+        for i in range(0,1):
+            print("1")
+
+    def MeanShiftTracker(parameters):
+        print("here")
+
+tracker=msTracker();
+tracker.MeanShiftSeek();
